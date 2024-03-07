2cb318dc9d8ccb3cf33f823b064f39a560cf237a
thien-an-ngo <t-a.ngo@micromata.de>
2024-03-07 23:35:43 +0100

    work in progress

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import '../app.css'
 	import { Nav } from '$lib/molecules';
+	import Logo from '$lib/assets/static/VictoryAn_logo.svg';
 </script>
+
+<svelte:head>
+	<link rel="icon" type="image/x-icon" href="{Logo}">
+	<title>
+		Juno
+	</title>
+</svelte:head>
 
 <Nav>
 </Nav>
---
+++ client/src/services/i18n/i18n.ts
@@ -0,0 +1,5 @@
+import { dictionary, locale, _ } from 'svelte-i18n';
+
+export const i18nSetup = ({ withLocale: _locale } = { withLocale: 'en' }) => {
+
+}
